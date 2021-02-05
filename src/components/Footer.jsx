11b784a98a85aebfc19ebace104f7418d64ed473
John Carlo Cunanan <codingjohn@outlook.com>
2021-02-05 18:26:49 +0800

    integrated with freegeoip and worldtimeapi

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+// context
+import {StatusContext} from "../contexts/StatusContext"
 
 const Footer = () => {
+  const { timeData } = useContext(StatusContext);
 
   return (
     <footer className="footer container">
       <div className="wrapper timezone-wrapper">
         <p>Current timezone</p>
-        <h4>Asia/Manila</h4>
+        <h4>{timeData.timezone}</h4>
       </div>
       <div className="wrapper dayYear-wrapper">
         <p>Day of the year</p>
-        <h4>36</h4>
+        <h4>{timeData.day_of_year}</h4>
       </div>
       <div className="wrapper dayWeek-wrapper">
         <p>day of the week</p>
-        <h4>5</h4>
+        <h4>{timeData.day_of_week}</h4>
       </div>
       <div className="wrapper weekNumber-wrapper">
         <p>week number</p>
-        <h4>5</h4>
+        <h4>{ timeData.week_number }</h4>
       </div>
     </footer> 
   )
